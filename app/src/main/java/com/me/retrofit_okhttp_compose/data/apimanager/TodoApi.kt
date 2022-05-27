804ecf72707de6495dfa65ff0549386c27fbd39f
canadianExperience <lena2157@mail.ru>
2022-05-27 13:47:42 -0400

    http header interceptor added to retrofit okhhtp client

--- app/src/main/java/com/me/retrofit_okhttp_compose/data/apimanager/TodoApi.kt
+++ app/src/main/java/com/me/retrofit_okhttp_compose/data/apimanager/TodoApi.kt
@@ -3,6 +3,7 @@ package com.me.retrofit_okhttp_compose.data.apimanager
 import com.me.retrofit_okhttp_compose.data.apimanager.model.Todo
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface TodoApi {
 //    https://jsonplaceholder.typicode.com/todos
--- app/src/main/java/com/me/retrofit_okhttp_compose/data/di/NetworkModule.kt
+++ app/src/main/java/com/me/retrofit_okhttp_compose/data/di/NetworkModule.kt
@@ -6,7 +6,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -18,10 +20,41 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideHttpClient() : OkHttpClient {
-        return OkHttpClient.Builder()
+//        return OkHttpClient.Builder()
+//            .readTimeout(15, TimeUnit.SECONDS)
+//            .connectTimeout(15, TimeUnit.SECONDS)
+//            .build()
+
+       val token: String? = "my_token"
+
+       val client = OkHttpClient.Builder()
+            .addInterceptor(Interceptor {chain ->
+                //HTTP header interceptor
+
+                //Task: add "Bearer" Authorization header to every call (request). Note
+                //that header should be added only if a token is present.
+                val request = chain.request()
+                if(request.header("No-Authentication") == null){
+                    //Check that user is authenticated
+                    if(!token.isNullOrEmpty()){
+                        //Check the token is present
+                        val finalToken = "Bearer $token"
+
+                        request
+                            .newBuilder()
+                            .addHeader("Authorization", finalToken)
+                            .build()
+                    }
+                }
+
+                val response = chain.proceed(request);
+                response
+            })
             .readTimeout(15, TimeUnit.SECONDS)
             .connectTimeout(15, TimeUnit.SECONDS)
-            .build()
+
+
+        return client.build()
     }
 
     @Singleton
